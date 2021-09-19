adb38775ea595d77b779eda70d4ad3907fe0ff74
Anderson <andersonecomp091@gmail.com>
2021-09-19 13:01:00 -0300

    adding customer own properties

--- src/entities/Customer.js
+++ src/entities/Customer.js
@@ -1,7 +1,8 @@
 const Base = require('./Base');
 class Customer extends Base {
-  constructor(){
-    
+  constructor({ id, name, age}) {
+    super({ id, name });
+    this.age = age;
   }
 }
 module.exports = Customer;
